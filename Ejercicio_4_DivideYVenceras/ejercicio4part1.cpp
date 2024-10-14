0889e803edf2b3794249458dd5845cb146d64676
Adrian <caradvm@gmail.com>
2024-10-14 02:32:01 -0600

    Create ejercicio4part1.cpp

---
+++ Ejercicio_4_DivideYVenceras/ejercicio4part1.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+struct Coordenada {
+    float x;
+    float y;
+};
+
+float generarNumeroAleatorio(float min, float max) {
+    return min + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (max - min)));
+}
+
+bool compararPorX(const Coordenada& a, const Coordenada& b) {
+    return a.x < b.x;
+}
+
+float calcularDistancia(const Coordenada& a, const Coordenada& b) {
+    return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2));
+}
+
+int main() {
+    srand(time(0)); 
+    int cantidad;
+
+    cout << "Ingresa la cantidad de coordenadas a generar: ";
+    cin >> cantidad;
+
+    vector<Coordenada> coordenadas;
+
+    for (int i = 0; i < cantidad; ++i) {
+        Coordenada coord;
+        coord.x = generarNumeroAleatorio(-10.0f, 10.0f);
+        coord.y = generarNumeroAleatorio(-10.0f, 10.0f);
+        coordenadas.push_back(coord);
+    }
+
+    sort(coordenadas.begin(), coordenadas.end(), compararPorX);
+
+    cout << fixed << setprecision(1);
+
+    cout << "\nCoordenadas generadas y ordenadas por X:\n";
+    for (const auto& coord : coordenadas) {
+        cout << "(" << coord.x << ", " << coord.y << ")\n";
+    }
+
+    float distanciaMinima = numeric_limits<float>::max();
+    Coordenada punto1, punto2;
+
+    for (size_t i = 0; i + 1 < coordenadas.size(); i += 2) {
+        float distancia = calcularDistancia(coordenadas[i], coordenadas[i + 1]);
+        if (distancia < distanciaMinima) {
+            distanciaMinima = distancia;
+            punto1 = coordenadas[i];
+            punto2 = coordenadas[i + 1];
+        }
+    }
+
+    cout << "\nLa distancia más corta es: " << distanciaMinima << "\n";
+    cout << "Entre los puntos (" << punto1.x << ", " << punto1.y << ") y ("
+         << punto2.x << ", " << punto2.y << ")\n";
+
+    return 0;
+}
+//O(nlogn)
